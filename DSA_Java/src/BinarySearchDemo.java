5ead5b481ac5b5767be8c7944f796b9ea6d65ffd
Gilberto Martini Madeira <gilberto.madeira@outlook.com>
2024-03-27 19:45:50 -0300

    Added interpolation search example

--- DSA_Java/src/BinarySearchDemo.java
+++ DSA_Java/src/BinarySearchDemo.java
@@ -26,18 +26,62 @@ public class BinarySearchDemo {
         else{
             System.out.println("Element not found.");
         }
+
         
+        System.out.println("Interpolation Search");
+
+        int[] secondArray = {1,2,3,14,5,6,7,8,9};
+
+        index = interpolationSearch(array, 8);
+
+        System.out.println(index);
 
 
     }
 
+
+    /**
+     * INterpolation Search - improvement over binary search best used for "uniformly" distributed data
+     * "guesses" where a value might be based on calculated probe results
+     * if probe is incorrect , search area is narrowed, and a new probe is calculated
+     * 
+     * average case: O(log(log(n)))
+     * worst case: O(n) [values increase exponentially]
+     * @return
+     */
+    private static int interpolationSearch(int[] array, int value) {
+        
+
+        int high = array.length - 1;
+        int low = 0;
+
+        while(value >= array[low] && value <= array[high] & low <= high ) {
+            int probe = low + (high - low) * (value - array[low]) 
+                            / ( array[high] - array[low] );
+
+            System.out.println("probe: " + probe);
+
+            if(array[probe] == value) return probe;
+            else if(array[probe] < value) low = probe + 1;
+            else if(array[probe] > value) high = probe - 1; // maintaining if condition for didactics purposes
+
+        
+        }
+
+
+
+        return 0;
+        
+    }
+
+
+     
     private static int binarySearch(int[] array, int target) {
         // TODO Auto-generated method stub
 
         int lo = 0;
         int hi = array.length -1;
-
-        
+    
 
         while(lo <= hi){
             int mid = lo + ( hi - lo) /2;
@@ -54,5 +98,8 @@ public class BinarySearchDemo {
         return -1; //target not found
         
     }
+
+   
+   
     
 }
