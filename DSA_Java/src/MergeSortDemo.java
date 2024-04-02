fbd5fea9e2af72342b5b1868700524f98099d1c4
Gilberto Martini Madeira <gilberto.madeira@outlook.com>
2024-04-02 20:01:44 -0300

    Added Merge sort

--- DSA_Java/src/MergeSortDemo.java
+++ DSA_Java/src/MergeSortDemo.java
@@ -11,13 +11,65 @@ public class MergeSortDemo {
 
 
     }
-
+    // merge sort = recursively divides array in 2, sort, re-combine
+    // run- time complexity = O(n log n)
+    // Space complexity  =   O(n)
     private static void mergeSort(int[] array) {
         
-        
+        int length = array.length;
+        if(length <= 1) return; // base case
+    
+        int middle  = length / 2;
+        int[] leftArray = new int[middle];
+        int[] rightArray = new int[length - middle];
+
+        int i = 0; //left array
+        int j = 0; //right array
+
+        for (; i < length; i++) {
+            if(i < middle){
+                leftArray[i] = array[i];
+                   
+            }else{
+                rightArray[j] = array[i];
+                j++;
+            }
+        }
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+        merge(leftArray, rightArray, array);
     }
 
-    private static void merge(){
-                
+    private static void merge(int[] leftArray, int[] rightArray, int[] array){
+
+        int leftSize = array.length / 2;
+        int rightSize = array.length - leftSize;
+        int i = 0, l = 0, r = 0;
+
+        //check the conditions for merging
+
+        while(l < leftSize && r < rightSize){
+            if(leftArray[l] < rightArray[r]){
+                array[i] = leftArray[l];
+                i++;
+                l++;            
+            }
+            else{
+                array[i] = rightArray[r];
+                i++;
+                r++;
+            }
+        }
+        while(l < leftSize){
+            array[i] = leftArray[l];
+            i++;
+            l++;
+        }
+        while(r < rightSize){
+            array[i] = rightArray[r];
+            i++;
+            r++;
+        }
+
     }
 }
